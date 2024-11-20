861f7b26adcdc40229325018e992cfa5a0a49c66
Yoscarx <163518352+Yoscarx@users.noreply.github.com>
2024-11-20 18:42:24 -0400

    Add files via upload

---
+++ OCESACNA/OCE_SACNA/OfficeLayer/User.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer;
+
+namespace OfficeLayer
+{
+    public class User : Entity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public AuthManager.RANKING Rank { get; set; }
+        public AuthManager.USER_STATE State { get; set; }
+
+        public User(string Username = "", string Password = "", AuthManager.RANKING Rank = AuthManager.RANKING.NONE, AuthManager.USER_STATE State = AuthManager.USER_STATE.NONE)
+        {
+            this.Username = Username;
+            this.Password = Password;
+            this.State = State;
+            this.Rank = Rank;
+        }
+    }
+}
