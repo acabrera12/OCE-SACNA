b72b961b42da1d5832b30ae50587295ea394758b
Chilos <149505603+Chilos2007@users.noreply.github.com>
2025-07-03 18:04:38 -0400

    refactorizando el método de entrada

--- View/Program.cs
+++ View/Program.cs
@@ -1,4 +1,5 @@
-﻿using OCESACNA.Controller;
+using OCESACNA.Controller;
+using OCESACNA.Core;
 using System;
 using System.Windows.Forms;
 using Settings = OCESACNA.View.Properties.Settings;
@@ -7,7 +8,7 @@ using Theme = OCESACNA.View.Collections.ColorTheme;
 namespace OCESACNA.View
 {
     /// <summary>
-    /// Programa
+    /// Representa el Programa
     /// </summary>
     static class Program
     {
@@ -27,56 +28,70 @@ namespace OCESACNA.View
         public static Menu.HandlerMenu Handler { get; set; }
 
         /// <summary>
+        /// Cantidad máxima de intentos de inicialización en caso de error
+        /// </summary>
+        private const int MaxRetries = 3;
+
+        /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // Pre-Inicialización de la aplicación
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            // Cargar el tema guardado en configuración
-            CurrentTheme = Theme.GetTheme((Theme.Themes)Settings.Theme);
-            CurrentTheme.SetDarkMode(Settings.ThemeDarkMode);
+            InitializeTheme();
 
-            // Inicializar controladores
-            bool Error = false, ContinueExecution = true;
+            bool Initialized = false;
+            int retryCount = 0;
 
-            try
+            while(!Initialized && retryCount < MaxRetries)
             {
-                DataController.Init(Settings.HostName, Settings.HostUser, Settings.HostPassword);
-                AuthController.Init();
-            }
-            catch (Exception err)
-            {
-                Error = true;
-                Handler = new Menu.HandlerMenu();
+                Initialized = Startup.Initialize(Settings, out var handler);
 
-                Console.WriteLine($"{typeof(Program)}:A Exception was catched during Initialization: {err.Message}");
-                MessageBox.Show($"Un error ha sido provocado durante la" +
-                                $"\ninicialización de la aplicación:" +
-                                $"\n{err.Message}", "Error de inicialización",
-                                MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Application.Run(Handler);
-            }
-
-            // Ejecución de la aplicación
-            if (Error)
-            {
-                if ((bool)!(Handler?.CloseOnExit))
+                if(!Initialized)
                 {
-                    ContinueExecution = false;
+                    Handler = handler;
+                    Application.Run(Handler);
+
+                    if((bool)(Handler?.CloseOnExit))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
                 }
             }
 
-            if (ContinueExecution)
+            if (Initialized)
             {
                 Application.Run(new Menu.AuthMenu());
             }
+            else if (retryCount >= MaxRetries)
+            {
+                MessageBox.Show(
+                    "Se alcanzó el número máximo de intentos de inicialización. \n" +
+                    "Por seguridad, el programa terminará la ejecución. \n" +
+                    "Puede volver a ejecutar el programa si lo desea.",
+                    "Error crítico",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
 
-            // Se ejecuta luego de cerrar la aplicación
             Settings.Save();
+        }
+
+        /// <summary>
+        /// Iniciliza el tema del programa cargando el valor guardado en la configuración
+        /// </summary>
+        private static void InitializeTheme()
+        {
+            CurrentTheme = Theme.GetTheme((Theme.Themes)Settings.Theme);
+            CurrentTheme.SetDarkMode(Settings.ThemeDarkMode);
         }
     }
 }
